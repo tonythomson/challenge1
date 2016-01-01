3d890f8bcbf060256bfb0ef032e60f17e715c741
Tony Thomson <thomson@me.com>
2016-01-01 08:26:54 -0800

    Sends player moves to server

--- challenge1.js
+++ challenge1.js
@@ -1,24 +1,34 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+
+  function checkWinner(player, move) {
+    Meteor.call('declareWinner', player, move, function(error,result) {
+    });
+  }
+
 
   Template.player.helpers({
-    player: function () {
+    player: function() {
       return Session.get('player');
     }
   });
 
   Template.player.events({
-    'click button': function (e) {
-      // increment the counter when button is clicked
-      Session.set('roshambo' + Session.get('player'), e.target.id);
-      console.log(e.target.id);
+    'click button': function(e) {
+      var player = Session.get('player'),
+        move = e.target.id;
+
+      Session.set('roshambo' + player, move);
+      checkWinner(player, move);
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.methods({
+    declareWinner: function(player, move){
+      console.log('Received move ' + move + ' from player ' + player);
+      return false;
+    },
   });
+
 }
