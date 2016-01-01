ae6418b06229ddb9aa50f413816cb624d10dc550
Tony Thomson <thomson@me.com>
2015-12-31 18:05:04 -0800

    Add state to differentiate players, event handlers for moves

--- challenge1.js
+++ challenge1.js
@@ -2,16 +2,17 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.player.helpers({
+    player: function () {
+      return Session.get('player');
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
+  Template.player.events({
+    'click button': function (e) {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      Session.set('roshambo' + Session.get('player'), e.target.id);
+      console.log(e.target.id);
     }
   });
 }
--- route.js
+++ route.js
@@ -5,10 +5,12 @@ Router.route('/', function() {
 
 Router.route('/player1', function() {
   'use strict';
+  Session.set('player', 1);
   this.render('player');
 });
 
 Router.route('/player2', function() {
   'use strict';
+    Session.set('player', 2);
   this.render('player');
 })
